cbfab6138a114f7612f722e8dc91970facf44386
Olawoyin Daniel <olawoyindaniel95@gmail.com>
2022-01-01 01:06:29 +0100

    started explore page revamp

--- src/components/layout/LandingPage/index.tsx
+++ src/components/layout/LandingPage/index.tsx
@@ -1,16 +1,26 @@
 import React from "react";
 import LandingPageHeader from "./Header";
 import LandingPageFooter from "./Footer";
+import { useRouter } from "next/router";
 
 interface Props {
   children: React.ReactNode;
 }
 
 const LandingPageLayout: React.FC<Props> = ({ children }) => {
+  const router = useRouter();
+  const pathname = router.pathname;
+  console.log(pathname);
   return (
     <>
       <LandingPageHeader />
-      <main className="page-container min-h-screen pt-8 pb-12">{children}</main>
+      <main
+        className={`${
+          pathname !== "/explore" && "page-container"
+        } min-h-screen pt-8 pb-12`}
+      >
+        {children}
+      </main>
       <LandingPageFooter />
     </>
   );
