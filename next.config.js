5aa24fe4ab2e88c350b126bc95b3b66a5211619e
Sdqumar <sdqumar09@gmail.com>
2023-09-07 07:59:09 +0100

    feat: fetch data from directus

--- next.config.js
+++ next.config.js
@@ -18,4 +18,7 @@ module.exports = {
   reactStrictMode: true,
   basePath: config.site.base_path !== "/" ? config.site.base_path : "",
   trailingSlash: config.site.trailing_slash,
+  images: {
+    domains: ["directus-admin-service-mr73ptziua-uc.a.run.app"],
+  },
 };
