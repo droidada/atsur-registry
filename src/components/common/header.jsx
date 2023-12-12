408088ec6443b996e3806b6d829a563481ef0e9a
Ada Orajiaku <adaorajiaku@gmail.com>
2023-12-12 23:39:54 +0000

    another build fix

--- src/components/common/header.jsx
+++ src/components/common/header.jsx
@@ -17,7 +17,7 @@ import logo from "@/assets/logo.jpeg";
 import Image from "next/image";
 import DropdownIcon from "../icons/DropdownIcon";
 import NotificationIcon from "../icons/NotificationIcon";
-import SideBar from "./sideBar";
+import SideBar from "./sidebar";
 import { useAuthContext } from "@/providers/auth.context";
 
 const Header = () => {
