5c719f5345bd708fc01d67215838ee0a57d9505f
samynaj <samuelnnj@gmail.com>
2024-03-02 23:57:32 +0100

    toast made bigger

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,37 +1,63 @@
 import { useState } from "react";
+import { useController } from "react-hook-form";
 
 type InputProps = {
   label?: string;
   type?: string;
   placeholder?: string;
   className?: string;
+  name?: string;
+  control?: any;
+  rules?: any;
+  defaultValue?: any;
 };
+
 const Input = ({
   label,
   type = "text",
   placeholder,
+  name,
+  control,
+  rules,
+  defaultValue,
   className,
   ...props
 }: InputProps) => {
-  const [value, setValue] = useState("");
-  const onChange = (e) => {
-    setValue(e.target.value);
-  };
+  // Destructure field and fieldState from useController
+  const {
+    field: { onChange, onBlur, value, ref },
+    fieldState: { invalid, error },
+  } = useController({
+    name,
+    control,
+    rules,
+  });
+
+  // Conditionally set initial value based on defaultValue
+  const initialValue = defaultValue ? defaultValue : "";
+
   return (
-    <div className="flex flex-col gap-2 mb-4">
-      <label className="text-[#252730] font-semibold" htmlFor={label}>
+    <div className="w-full flex flex-col gap-2 mb-4">
+      <label className="text-white font-semibold" htmlFor={label}>
         {label}
       </label>
 
       <input
         type={type}
-        name={label}
-        value={value}
+        placeholder={defaultValue ? defaultValue : placeholder}
         onChange={onChange}
-        placeholder={placeholder}
-        className={`border border-[#D5D6DE] focus:border-0 focus:outline-0 rounded-lg p-2 w-[441px] px-[14px] py-[12px]  ${className}`}
+        onBlur={onBlur}
+        // Use initialValue if value is empty (on first render)
+        value={value === "" ? initialValue : value}
+        ref={ref}
+        className={`border border-[#D5D6DE] focus:border-0 focus:outline-0 w-[441px] h-[50px] px-[14px] py-[12px]  ${className}`}
         {...props}
       />
+      {invalid && (
+        <span className="text-xs text-red-500 font-extralight">
+          {error?.message}
+        </span>
+      )}
     </div>
   );
 };
