916c0a7817edcb5f58f051f455ab088f401b62bc
Ada Orajiaku <adaorajiaku@gmail.com>
2024-04-05 17:46:36 +0100

    fixing session err in prod

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,30 +3,30 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import { signIn } from "next-auth/react";
 import dotenv from "dotenv";
 import axios from "axios";
-import Cookies from "cookies";
+//import Cookies from "cookies";
 
-let cookies;
+// let cookies;
 
-const setRefreshCookie = ({ cookies, accessToken, refreshToken }) => {
-  const date = new Date();
-  const time = date.getTime();
-  const expireTime = time + 24 * 60 * 60 * 1000 * 30; // 30 days
-  date.setTime(expireTime);
+// const setRefreshCookie = ({ cookies, accessToken, refreshToken }) => {
+//   const date = new Date();
+//   const time = date.getTime();
+//   const expireTime = time + 24 * 60 * 60 * 1000 * 30; // 30 days
+//   date.setTime(expireTime);
 
-  cookies.set("refreshToken", refreshToken, {
-    sameSite: "strict",
-    overwrite: true,
-    expires: date,
-    httpOnly: true,
-  });
+//   cookies.set("refreshToken", refreshToken, {
+//     sameSite: "strict",
+//     overwrite: true,
+//     expires: date,
+//     httpOnly: true,
+//   });
 
-  cookies.set("accessToken", accessToken, {
-    sameSite: "strict",
-    overwrite: true,
-    expires: date,
-    httpOnly: true,
-  });
-};
+//   cookies.set("accessToken", accessToken, {
+//     sameSite: "strict",
+//     overwrite: true,
+//     expires: date,
+//     httpOnly: true,
+//   });
+// };
 
 dotenv.config();
 
@@ -80,11 +80,11 @@ export const options: any = {
       console.log(`token: ${token}`);
       if (account && user) {
         console.log(`account: ${account} && user ${user}`);
-        setRefreshCookie({
-          cookies,
-          accessToken: account.access_token,
-          refreshToken: account.refresh_token,
-        });
+        // setRefreshCookie({
+        //   cookies,
+        //   accessToken: account.access_token,
+        //   refreshToken: account.refresh_token,
+        // });
         return {
           ...token,
           user: user,
@@ -95,7 +95,7 @@ export const options: any = {
         return token;
       }
 
-      return await refreshAccessToken(token);
+      return null;
     },
 
     async session({ session, token }) {
@@ -103,9 +103,9 @@ export const options: any = {
       console.log(
         `session: ${JSON.stringify(session)}  token: ${JSON.stringify(token)}`,
       );
-      session.user.accessToken = token?.user.accessToken;
-      session.user.refreshToken = token?.user.user.refreshToken;
-      session.user.expires = token?.user.user.expires;
+      session.user.accessToken = token?.user?.accessToken;
+      session.user.refreshToken = token?.user?.refreshToken;
+      session.user.expires = token?.exp;
       session.user.error = token?.error;
 
       return session;
@@ -118,49 +118,49 @@ export const options: any = {
   debug: true,
 };
 
-async function refreshAccessToken(token) {
-  try {
-    const res = await fetch(pubAPI + "/auth/refresh-token", {
-      method: "POST",
-      body: JSON.stringify({
-        refreshToken: token?.refreshToken,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-      credentials: "include",
-    });
-    const response = await res.json();
+// async function refreshAccessToken(token) {
+//   try {
+//     const res = await fetch(pubAPI + "/auth/refresh-token", {
+//       method: "POST",
+//       body: JSON.stringify({
+//         refreshToken: token?.refreshToken,
+//       }),
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       credentials: "include",
+//     });
+//     const response = await res.json();
 
-    console.log("refreshed tokens ", response);
+//     console.log("refreshed tokens ", response);
 
-    if (!response.ok) {
-      signIn();
-    }
+//     if (!response.ok) {
+//       signIn();
+//     }
 
-    if (response) {
-      return {
-        ...token,
-        accessToken: response?.accessToken,
-        expires: Date.now() + response?.expires,
-        refreshToken: response?.refreshToken,
-      };
-    }
-  } catch (error) {
-    console.log(
-      new Date().toUTCString() + " Error in refreshAccessToken:",
-      error,
-    );
+//     if (response) {
+//       return {
+//         ...token,
+//         accessToken: response?.accessToken,
+//         expires: Date.now() + response?.expires,
+//         refreshToken: response?.refreshToken,
+//       };
+//     }
+//   } catch (error) {
+//     console.log(
+//       new Date().toUTCString() + " Error in refreshAccessToken:",
+//       error,
+//     );
 
-    return {
-      ...token,
-      error: "RefreshAccessTokenError",
-    };
-  }
-}
+//     return {
+//       ...token,
+//       error: "RefreshAccessTokenError",
+//     };
+//   }
+// }
 
 const nextauthfunc = (req, res) => {
-  cookies = new Cookies(req, res);
+//cookies = new Cookies(req, res);
   return NextAuth(req, res, options);
 };
 
