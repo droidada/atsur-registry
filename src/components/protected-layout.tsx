903d639934665265436e6ea33474f6ca90ff470d
Ada Orajiaku <adaorajiaku@gmail.com>
2024-04-08 03:29:54 +0100

    fixed loading bugs, added analytics

--- src/components/protected-layout.tsx
+++ src/components/protected-layout.tsx
@@ -1,6 +1,7 @@
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import { Backdrop, CircularProgress } from "@mui/material";
 
 type Props = {
   children: React.ReactElement;
@@ -30,10 +31,24 @@ export const ProtectedLayout = ({ children }: Props): JSX.Element => {
 
   // if the user refreshed the page or somehow navigated to the protected page
   if (loading) {
-    return <>Loading app...</>;
+    return (
+      <Backdrop
+        sx={{ background:"#000", color: "#000", zIndex: (theme) => theme.zIndex.drawer + 1}}
+        open
+      >
+        <CircularProgress color="info" />
+      </Backdrop>
+    );
   }
 
   // if the user is authorized, render the page
   // otherwise, render nothing while the router redirects him to the login page
-  return authorized ? <div>{children}</div> : <></>;
+  return authorized ?
+        <div>{children}</div> :
+        <Backdrop
+        sx={{ background:"#000", color: "#000", zIndex: (theme) => theme.zIndex.drawer + 1}}
+        open
+      >
+        <CircularProgress color="info" />
+      </Backdrop>;
 };
