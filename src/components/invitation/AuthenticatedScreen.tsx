87e1bfe656b02cdd296795d4497604cfc8a86d85
Olawoyin Daniel <olawoyindaniel95@gmail.com>
2024-04-18 00:01:19 +0100

    started integrating invitaion model on the fe

---
+++ src/components/invitation/AuthenticatedScreen.tsx
@@ -0,0 +1,162 @@
+import useAxiosAuth from "@/hooks/useAxiosAuth";
+import { useToast } from "@/providers/ToastProvider";
+import { LoadingButton } from "@mui/lab";
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
+import { set } from "date-fns";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+
+interface Props {
+  type: string;
+  inviter: { firstName: string; lastName: string };
+  data: any;
+  objectType: "art-piece" | "org";
+}
+const AuthenticatedScreen = ({ type, inviter, data, objectType }: Props) => {
+  const [open, setOpen] = useState(false);
+  const [acceptLoading, setAcceptLoading] = useState(false);
+  const [rejectLoading, setRejectLoading] = useState(false);
+  const axiosFetch = useAxiosAuth();
+  const router = useRouter();
+  const { token } = router.query;
+
+  const toast = useToast();
+  const [message, setMessage] = useState("");
+
+  console.log(type);
+
+  const handleAccept = async () => {
+    try {
+      setAcceptLoading(true);
+      const { data } = await axiosFetch.post("/invite/accept", {
+        token,
+        accept: true,
+      });
+      toast.success("Successfully accepted the invitation!");
+      router.push("/dashboard");
+    } catch (error) {
+      toast.error("Error accepting the invitation!");
+      console.log(error);
+    } finally {
+      setAcceptLoading(false);
+    }
+  };
+
+  const handleReject = async () => {
+    try {
+      setRejectLoading(true);
+      const { data } = await axiosFetch.post("/invite/accept", {
+        token,
+        accept: false,
+      });
+      toast.success("Successfully rejected the invitation!");
+      router.push("/dashboard");
+    } catch (error) {
+      toast.error("Error accepting the invitation!");
+      console.log(error);
+    } finally {
+      setRejectLoading(false);
+    }
+  };
+
+  console.log(data);
+
+  useEffect(() => {
+    switch (type) {
+      case "user":
+        setMessage("join our platform as a user");
+        break;
+      case "art-piece-collaborator":
+        setMessage(` collaborate on "${data?.title}"`);
+        break;
+      case "org-collaborator":
+        setMessage(` collaborate on "${data?.name}" organization`);
+        break;
+      default:
+        setMessage(` join our platform `);
+    }
+  }, [type]);
+
+  return (
+    <div className="container mx-auto flex flex-col items-center gap-5 p-10 min-h-screen">
+      <h2>Invitation</h2>
+      <p>
+        {inviter?.firstName} {inviter?.lastName} invited you to {message}. Do
+        you want to accept the invitation?
+      </p>
+
+      <div className="flex gap-3">
+        <Button
+          onClick={() => setOpen(true)}
+          variant="contained"
+          className="style-1 rounded"
+        >
+          {type == "user"
+            ? "View Inviter"
+            : type == "org-collaborator"
+            ? "View Organization"
+            : "View Art Piece"}
+        </Button>
+        <LoadingButton
+          loading={acceptLoading}
+          variant="contained"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={handleAccept}
+        >
+          Accept
+        </LoadingButton>
+        <LoadingButton
+          loading={rejectLoading}
+          variant="contained"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={handleReject}
+        >
+          Reject
+        </LoadingButton>
+      </div>
+
+      <Dialog open={open} onClose={() => setOpen(false)}>
+        <DialogTitle id="alert-dialog-title">
+          {type == "user"
+            ? "View Inviter"
+            : type == "org-collaborator"
+            ? "View Organization"
+            : "View Art Piece"}
+        </DialogTitle>
+        <DialogContent>
+          {type == "user" ? (
+            <DialogContentText id="alert-dialog-description">
+              {inviter?.firstName} {inviter?.lastName}
+            </DialogContentText>
+          ) : type == "org-collaborator" ? (
+            ``
+          ) : (
+            <div className="flex flex-col items-center gap-7">
+              <Image
+                src={data?.assets[0]?.url}
+                alt={data?.title}
+                width={200}
+                height={200}
+              />
+              <DialogContentText id="alert-dialog-description">
+                {data?.title}
+              </DialogContentText>
+              <DialogContentText id="alert-dialog-description">
+                {data?.description}
+              </DialogContentText>
+            </div>
+          )}
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
+
+export default AuthenticatedScreen;
--- src/pages/dashboard/security/verify-document/status.tsx
+++ src/pages/dashboard/security/verify-document/status.tsx
@@ -1,19 +1,21 @@
 import axios from "@/lib/axios";
 import { getToken } from "next-auth/jwt";
-import React from "react";
+import React, { useState } from "react";
 import dynamic from "next/dynamic";
 
 import DashboardLayoutWithSidebar, {
   DashboardPages,
 } from "@/components/open9/layout/DashboardLayoutWithSidebar";
 import { useRouter } from "next/router";
+import useAxiosAuth from "@/hooks/useAxiosAuth";
+import { LoadingButton } from "@mui/lab";
 export const getServerSideProps = async ({ req, query }) => {
   try {
     const token: any = await getToken({
       req,
       secret: process.env.NEXTAUTH_SECRET,
     });
-    console.log("token here is ", token);
+
     if (!token) return;
 
     const res = await axios.get(`/smile-verification/status`, {
@@ -28,23 +30,40 @@ export const getServerSideProps = async ({ req, query }) => {
 const Status = ({ status }) => {
   console.log(status);
   const router = useRouter();
+  const axiosAuth = useAxiosAuth();
+  const [loading, setLoading] = useState(false);
+
+  const handleReenroll = async () => {
+    try {
+      setLoading(true);
+      const { data } = await axiosAuth.post("/smile-verification/re-enrol");
+      router.push("/dashboard/security/verify-document");
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <DashboardLayoutWithSidebar activePage={DashboardPages.SECURITY}>
       <h1>KYC Verification Status</h1>
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="container mx-auto grid gap-4 mt-6 px-4 sm:px-6 lg:px-8">
         <div>
-          Status: <h3 className=" font-bold">{status?.verificationStatus}</h3>
+          Status: <h4 className=" font-bold">{status?.verificationStatus}</h4>
         </div>
         <div>
           {status.verificationStatus === "rejected" ? "Reason" : "Message"}:{" "}
-          <h3 className=" font-bold">{status?.resultText}</h3>
+          <h4 className=" font-bold">{status?.resultText}</h4>
         </div>
-        {status.verificationStatus === "rejected" && (
-          <button
-            onClick={() => router.push("/dashboard/security/verify-document")}
+        {status.verificationStatus !== "verified" && (
+          <LoadingButton
+            loading={loading}
+            className="style-1"
+            onClick={handleReenroll}
           >
             Redo Verification
-          </button>
+          </LoadingButton>
         )}
       </div>
     </DashboardLayoutWithSidebar>
