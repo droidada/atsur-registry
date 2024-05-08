56f06179f80f2dff3a544efe2449e8fc76090110
Olawoyin Daniel <olawoyindaniel95@gmail.com>
2024-05-08 19:25:24 +0100

    started payment integration

--- src/components/pricing/PricingCard.tsx
+++ src/components/pricing/PricingCard.tsx
@@ -2,6 +2,13 @@ import { Button } from "@mui/material";
 import React from "react";
 import { IoCheckmarkSharp } from "react-icons/io5";
 import { HiMiniCheckCircle } from "react-icons/hi2";
+import { useMutation } from "@tanstack/react-query";
+import axios from "@/lib/axios";
+import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
+import { useToast } from "@/providers/ToastProvider";
+import { LoadingButton } from "@mui/lab";
+import useAxiosAuth from "@/hooks/useAxiosAuth";
 
 interface PricingCardProps {
   title: string;
@@ -20,6 +27,32 @@ const PricingCard: React.FC<PricingCardProps> = ({
   interval,
   isGreenButton,
 }) => {
+  const { status, data: userData } = useSession();
+  const router = useRouter();
+  const toast = useToast();
+  const axiosAuth = useAxiosAuth();
+
+  const { mutate, isLoading } = useMutation({
+    mutationFn: () =>
+      status === "unauthenticated"
+        ? router.push("/login?callbackUrl=/pricing")
+        : axiosAuth.post("/payment/initialize-transaction-with-plan", {
+            plan: "PLN_exlh5mpat92dwq8",
+            email: userData?.user?.email,
+            amount: 3000,
+          }),
+    onError: (error: any) => {
+      const errorMessage =
+        error.response?.data?.message ||
+        error.message ||
+        "Something Went wrong";
+      toast.error(errorMessage);
+    },
+    onSuccess: (data: any) => {
+      router.push(data?.data?.transaction?.authorization_url);
+    },
+  });
+
   return (
     <div className="max-w-[348.19px] w-full border-[1px] border-primary flex flex-col">
       <div className="bg-white flex flex-col items-center gap-8 px-8 py-10">
@@ -41,15 +74,18 @@ const PricingCard: React.FC<PricingCardProps> = ({
           </div>
         )}
 
-        <Button
-          className={`text-sm font-[400] p-3 ${
+        <LoadingButton
+          loading={isLoading}
+          loadingPosition="start"
+          onClick={() => mutate()}
+          className={`text-sm font-[400] px-3 ${
             isGreenButton
               ? "bg-[#00FF94] text-primary"
               : "bg-primary text-white"
           }`}
         >
           Get Started
-        </Button>
+        </LoadingButton>
       </div>
       <div className="bg-primary flex-1 flex flex-col items-start gap-4 px-8 py-10">
         {features.map((feature) => (
