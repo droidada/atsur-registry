aa843077120e57be57092c1d1252edb98bd124bf
Ada Orajiaku <adaorajiaku@gmail.com>
2024-05-13 11:41:03 +0100

    adding admin stuff

---
+++ src/pages/admin/users/index.tsx
@@ -0,0 +1,119 @@
+import { useState } from "react";
+import Link from "next/link";
+import useAxiosAuth from "@/hooks/useAxiosAuth";
+import { useQuery } from "@tanstack/react-query";
+import Layout from "@/open9/layout/Layout";
+import AdminDashboardLayout from "@/components/layout/AdminDashboardLayout";
+import { Paper,Table,TableBody,TableCell,TableContainer,TableHead,TablePagination,TableRow } from '@mui/material';
+
+interface Column {
+    id: 'firstName' | 'lastName' | 'email' | 'size' | 'density';
+    label: string;
+    minWidth?: number;
+    align?: 'right';
+    format?: (value: number) => string;
+  }
+
+  const columns: readonly Column[] = [
+    { id: 'firstName', label: 'First Name', minWidth: 170 },
+    { id: 'lastName', label: 'Last Name', minWidth: 170 },
+    { id: 'email', label: 'Email', minWidth: 170 },
+    {
+      id: 'size',
+      label: 'Size\u00a0(km\u00b2)',
+      minWidth: 170,
+      align: 'right',
+      format: (value: number) => value.toLocaleString('en-US'),
+    },
+    {
+      id: 'density',
+      label: 'Density',
+      minWidth: 170,
+      align: 'right',
+      format: (value: number) => value.toFixed(2),
+    },
+  ];
+
+
+export default function UserList() {
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(15);
+
+    const axiosFetch = useAxiosAuth();
+    const {
+      data: rows,
+      isFetching,
+      isError,
+    } = useQuery(["organizations"], () =>
+      axiosFetch.get(`/user/list?limit=${rowsPerPage}&skip=${page}`).then((res) => {
+        console.log(res.data);
+        return res.data?.users;
+      }),
+    );
+
+    const handleChangePage = (event: unknown, newPage: number) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setRowsPerPage(+event.target.value);
+        setPage(0);
+    };
+
+  return (
+    // eslint-disable-next-line react/jsx-no-comment-textnodes
+    <>
+      {/* // TODO add layout */}
+      <AdminDashboardLayout>
+        <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+              <TableContainer >
+                  <Table stickyHeader aria-label="sticky table">
+                  <TableHead>
+                      <TableRow>
+                      {columns.map((column) => (
+                          <TableCell
+                          key={column.id}
+                          align={column.align}
+                          style={{ minWidth: column.minWidth }}
+                          >
+                          {column.label}
+                          </TableCell>
+                      ))}
+                      </TableRow>
+                  </TableHead>
+                  <TableBody>
+                      {rows && rows
+                      .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                      .map((row) => {
+                          return (
+                          <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                              {columns.map((column) => {
+                              const value = row[column.id];
+                              return (
+                                  <TableCell key={column.id} align={column.align}>
+                                  {column.format && typeof value === 'number'
+                                      ? column.format(value)
+                                      : value}
+                                  </TableCell>
+                              );
+                              })}
+                          </TableRow>
+                          );
+                      })}
+                  </TableBody>
+                  </Table>
+              </TableContainer>
+              <TablePagination
+                  rowsPerPageOptions={[10, 25, 100]}
+                  component="div"
+                  count={rows?.length || 0}
+                  rowsPerPage={rowsPerPage || 10}
+                  page={page || 0}
+                  onPageChange={handleChangePage}
+                  onRowsPerPageChange={handleChangeRowsPerPage}
+              />
+        </Paper>
+      </AdminDashboardLayout>
+    </>
+  );
+}
