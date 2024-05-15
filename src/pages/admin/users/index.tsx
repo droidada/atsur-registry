52a081090ceba0b857f6f7ad98326eaef62e4924
Olawoyin Daniel <olawoyindaniel95@gmail.com>
2024-05-15 20:53:17 +0100

    added actions to the admin dashboard

--- src/pages/admin/users/index.tsx
+++ src/pages/admin/users/index.tsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
 import Link from "next/link";
 import useAxiosAuth from "@/hooks/useAxiosAuth";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import Layout from "@/open9/layout/Layout";
 import AdminDashboardLayout from "@/components/layout/AdminDashboardLayout";
 import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  Fade,
+  IconButton,
+  Menu,
+  MenuItem,
   Paper,
   Table,
   TableBody,
@@ -13,10 +20,13 @@ import {
   TableHead,
   TablePagination,
   TableRow,
+  Typography,
 } from "@mui/material";
+import { IoMdMore } from "react-icons/io";
+import { useToast } from "@/providers/ToastProvider";
 
 interface Column {
-  id: "firstName" | "lastName" | "email" | "size" | "density";
+  id: "firstName" | "lastName" | "email" | "status" | "actions" | "roles";
   label: string;
   minWidth?: number;
   align?: "right";
@@ -27,39 +37,110 @@ const columns: readonly Column[] = [
   { id: "firstName", label: "First Name", minWidth: 170 },
   { id: "lastName", label: "Last Name", minWidth: 170 },
   { id: "email", label: "Email", minWidth: 170 },
-  {
-    id: "size",
-    label: "Size\u00a0(km\u00b2)",
-    minWidth: 170,
-    align: "right",
-    format: (value: number) => value.toLocaleString("en-US"),
-  },
-  {
-    id: "density",
-    label: "Density",
-    minWidth: 170,
-    align: "right",
-    format: (value: number) => value.toFixed(2),
-  },
+  { id: "status", label: "Status", minWidth: 170 },
+  { id: "roles", label: "Roles", minWidth: 170 },
+  // {
+  //   id: "size",
+  //   label: "Size\u00a0(km\u00b2)",
+  //   minWidth: 170,
+  //   align: "right",
+  //   format: (value: number) => value.toLocaleString("en-US"),
+  // },
+  // {
+  //   id: "density",
+  //   label: "Density",
+  //   minWidth: 170,
+  //   align: "right",
+  //   format: (value: number) => value.toFixed(2),
+  // },
+  { id: "actions", label: "Actions", minWidth: 170, align: "right" },
 ];
 
 export default function UserList() {
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(15);
+  const [openDetailsDialog, setOpenDetailsDialog] = useState(false);
+  const [menuState, setMenuState] = useState({ anchorEl: null, rowId: null });
+  const [currentUser, setCurrentUser] = useState<any>(null);
+  const toast = useToast();
 
   const axiosFetch = useAxiosAuth();
   const {
     data: rows,
     isFetching,
     isError,
+    refetch,
   } = useQuery(["organizations"], () =>
     axiosFetch
       .get(`/user/list?limit=${rowsPerPage}&skip=${page}`)
       .then((res) => {
-        console.log(res.data);
         return res.data?.users;
       }),
   );
+
+  const { isLoading: suspendLoading, mutate: suspendUser } = useMutation({
+    mutationFn: (id) => axiosFetch.post(`/user/suspend/${id}`),
+    onSuccess: () => {
+      toast.success("User account is suspended");
+    },
+    onError: (error: any) => {
+      toast.error(
+        error?.response?.data?.message ||
+          error?.message ||
+          "Something went wrong",
+      );
+    },
+  });
+
+  const { isLoading: activateLoading, mutate: activateUser } = useMutation({
+    mutationFn: (id) => axiosFetch.post(`/user/activate/${id}`),
+    onSuccess: () => {
+      toast.success("User account is activated");
+    },
+    onError: (error: any) => {
+      toast.error(
+        error?.response?.data?.message ||
+          error?.message ||
+          "Something went wrong",
+      );
+    },
+  });
+
+  const { mutate: makeAdmin } = useMutation({
+    mutationFn: (id) => axiosFetch.post(`/user/make-admin/${id}`),
+    onSuccess: () => {
+      toast.success("User is now an admin");
+    },
+    onError: (error: any) => {
+      toast.error(
+        error?.response?.data?.message ||
+          error?.message ||
+          "Something went wrong",
+      );
+    },
+  });
+
+  const { mutate: removeAdmin } = useMutation({
+    mutationFn: (id) => axiosFetch.post(`/user/remove-admin/${id}`),
+    onSuccess: () => {
+      toast.success("User is no longer an admin");
+    },
+    onError: (error: any) => {
+      toast.error(
+        error?.response?.data?.message ||
+          error?.message ||
+          "Something went wrong",
+      );
+    },
+  });
+
+  const open = Boolean(menuState.anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>, rowId: string) => {
+    setMenuState({ anchorEl: event.currentTarget, rowId });
+  };
+  const handleClose = () => {
+    setMenuState({ anchorEl: null, rowId: null });
+  };
 
   const handleChangePage = (event: unknown, newPage: number) => {
     setPage(newPage);
@@ -70,6 +151,36 @@ export default function UserList() {
   ) => {
     setRowsPerPage(+event.target.value);
     setPage(0);
+  };
+
+  const handleSuspendUser = async (user_id: string, suspended: boolean) => {
+    if (suspended) {
+      // @ts-ignore
+      activateUser(user_id);
+    } else {
+      // @ts-ignore
+      suspendUser(user_id);
+    }
+    await refetch();
+    handleClose();
+  };
+
+  const handleMakeAdmin = async (user_id: string, isAdmin: boolean) => {
+    if (isAdmin) {
+      // @ts-ignore
+      removeAdmin(user_id);
+    } else {
+      // @ts-ignore
+      makeAdmin(user_id);
+    }
+    await refetch();
+    handleClose();
+  };
+
+  const handleViewDetails = (data) => {
+    setCurrentUser(data);
+    setOpenDetailsDialog(true);
+    handleClose();
   };
 
   return (
@@ -103,15 +214,82 @@ export default function UserList() {
                           hover
                           role="checkbox"
                           tabIndex={-1}
-                          key={row.code}
+                          key={row?.id}
                         >
                           {columns.map((column) => {
                             const value = row[column.id];
                             return (
                               <TableCell key={column.id} align={column.align}>
-                                {column.format && typeof value === "number"
-                                  ? column.format(value)
-                                  : value}
+                                {column.id === "actions" ? (
+                                  <>
+                                    <IconButton
+                                      id="fade-button"
+                                      aria-controls={
+                                        open ? "fade-menu" : undefined
+                                      }
+                                      aria-haspopup="true"
+                                      aria-expanded={open ? "true" : undefined}
+                                      onClick={(event) =>
+                                        handleClick(event, row.id)
+                                      }
+                                    >
+                                      <IoMdMore />
+                                    </IconButton>
+                                    <Menu
+                                      id="fade-menu"
+                                      MenuListProps={{
+                                        "aria-labelledby": "fade-button",
+                                      }}
+                                      anchorEl={menuState.anchorEl}
+                                      open={open && menuState.rowId === row.id}
+                                      onClose={handleClose}
+                                      TransitionComponent={Fade}
+                                    >
+                                      <MenuItem
+                                        onClick={() =>
+                                          handleSuspendUser(
+                                            row?.id,
+                                            row?.suspended,
+                                          )
+                                        }
+                                      >
+                                        {row?.suspended
+                                          ? "Activate"
+                                          : "Suspend"}
+                                      </MenuItem>
+                                      <MenuItem
+                                        onClick={() =>
+                                          handleMakeAdmin(
+                                            row?.id,
+                                            row?.roles?.includes("admin"),
+                                          )
+                                        }
+                                      >
+                                        {row?.roles?.includes("admin")
+                                          ? "Remove Admin"
+                                          : "Make Admin"}
+                                      </MenuItem>
+                                      <MenuItem
+                                        onClick={() => handleViewDetails(row)}
+                                      >
+                                        View Details
+                                      </MenuItem>
+                                    </Menu>
+                                  </>
+                                ) : column.format &&
+                                  typeof value === "number" ? (
+                                  column.format(value)
+                                ) : column.id === "status" ? (
+                                  row?.suspended ? (
+                                    "Suspended"
+                                  ) : (
+                                    "Active"
+                                  )
+                                ) : column.id === "roles" ? (
+                                  row?.roles?.join(", ")
+                                ) : (
+                                  value
+                                )}
                               </TableCell>
                             );
                           })}
@@ -132,6 +310,47 @@ export default function UserList() {
           />
         </Paper>
       </AdminDashboardLayout>
+
+      <UserDetailsDialog
+        userData={currentUser}
+        handleClose={() => {
+          setOpenDetailsDialog(false);
+          setCurrentUser(null);
+        }}
+        open={openDetailsDialog}
+      />
     </>
   );
 }
+
+const UserDetailsDialog = ({ userData, handleClose, open }) => {
+  return (
+    <Dialog
+      PaperComponent={Paper}
+      fullWidth={true}
+      maxWidth={"sm"}
+      open={open}
+      onClose={handleClose}
+    >
+      <DialogTitle>User Details</DialogTitle>
+      <DialogContent dividers>
+        <Typography gutterBottom>
+          <b>Name:</b> {userData?.firstName} {userData?.lastName}
+        </Typography>
+        <Typography gutterBottom>
+          <b>Email:</b> {userData?.email}
+        </Typography>
+        <Typography gutterBottom>
+          <b>Roles:</b> {userData?.roles.join(", ")}
+        </Typography>
+        <Typography gutterBottom>
+          <b>Status:</b> {userData?.suspended ? "Suspended" : "Active"}
+        </Typography>
+        <Typography gutterBottom>
+          <b>Verification Status:</b>{" "}
+          {userData?.kycVerification?.verificationStatus.replace(/-/g, " ")}
+        </Typography>
+      </DialogContent>
+    </Dialog>
+  );
+};
