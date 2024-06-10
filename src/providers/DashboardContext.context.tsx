cbe3a330e6c846a6367b860f041ee1a4e5c9e312
Olawoyin Daniel <olawoyindaniel95@gmail.com>
2024-06-10 12:40:03 +0100

    fixed all verification issues

---
+++ src/providers/DashboardContext.context.tsx
@@ -0,0 +1,77 @@
+import useAxiosAuth from "@/hooks/useAxiosAuth";
+import { useQuery } from "@tanstack/react-query";
+import { createContext, useEffect, useState } from "react";
+
+interface DashboardContextProps {
+  notifications: any;
+  setNotifications: React.Dispatch<React.SetStateAction<any>>;
+  credits: any;
+  setCredits: React.Dispatch<React.SetStateAction<any>>;
+}
+
+export const dashboardContext = createContext<DashboardContextProps>({
+  notifications: [],
+  setNotifications: () => {},
+  credits: [],
+  setCredits: () => {},
+});
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const DashboardContextProvider = ({ children }: Props) => {
+  const [notifications, setNotifications] = useState([]);
+  const [credits, setCredits] = useState([]);
+
+  const axiosAuth = useAxiosAuth();
+
+  const {
+    data: getCredits,
+    isLoading: isLoadingCredits,
+    error: errorCredits,
+    isFetched: isFetchedCredits,
+  } = useQuery({
+    queryKey: ["credits"],
+    queryFn: async () => {
+      const { data: response } = await axiosAuth.get("/user/credits");
+      console.log(response.data?.credits);
+      return response.data?.credits;
+    },
+  });
+
+  const {
+    data: getNotifications,
+    isLoading: isLoadingNotifications,
+    error: errorNotifications,
+    isFetched: isFetchedNotifications,
+  } = useQuery({
+    queryFn: async () => {
+      const response = await axiosAuth.get("/notifications");
+      return response.data;
+    },
+    queryKey: ["notifications"],
+  });
+
+  useEffect(() => {
+    if (getCredits) {
+      setCredits(getCredits);
+    }
+  }, [isFetchedNotifications]);
+
+  useEffect(() => {
+    if (getNotifications) {
+      setNotifications(getNotifications);
+    }
+  }, [isFetchedNotifications]);
+
+  return (
+    <dashboardContext.Provider
+      value={{ notifications, setNotifications, credits, setCredits }}
+    >
+      {children}
+    </dashboardContext.Provider>
+  );
+};
+
+export default DashboardContextProvider;
