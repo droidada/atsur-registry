be25143b9291d93076b82ae6be94b0fce366018a
Olawoyin Daniel <olawoyindaniel95@gmail.com>
2024-07-23 11:06:08 -0700

    fixed pending ui design

--- src/components/dashboard/artwork/Verification/VerificationPending/index.tsx
+++ src/components/dashboard/artwork/Verification/VerificationPending/index.tsx
@@ -2,6 +2,7 @@ import { Button } from "@mui/material";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import React from "react";
+import { GrCheckmark } from "react-icons/gr";
 
 interface Props {
   artPiece: any;
@@ -12,7 +13,7 @@ const VerificationPending: React.FC<Props> = ({ artPiece }) => {
   console.log(artPiece);
 
   return (
-    <div className="flex lg:flex-nowrap justify-center items-center mt-6 flex-wrap md:gap-8">
+    <div className="flex lg:flex-nowrap justify-center  items-center mt-6 flex-wrap md:gap-8">
       <div
         style={{ aspectRatio: "1/1" }}
         className="relative md:w-[330px] w-[250px] bg-secondary rounded-full overflow-hidden"
@@ -24,19 +25,13 @@ const VerificationPending: React.FC<Props> = ({ artPiece }) => {
           className="object-cover"
         />
       </div>
-      <div className="flex lg:w-1/2 flex-col gap-4">
-        <h2 className="text-3xl text-[#4C4C4C] md:text-6xl  lg:text-8xl font-bold">
+      {/* <div className="flex items-center  gap-8"> */}
+      <div className="flex max-w-[266px] w-full  flex-col gap-4">
+        <h2 className="text-3xl text-[#4C4C4C] md:text-[57px] font-bold">
           Pending
         </h2>
         <p className="w-full   text-justify text-[17px] leading-[20px] font-[300]">
-          Your art piece has been published and awaiting verification. This will
-          take a few days. Meanwhile, if you have not done your KYC
-          verification, click on the button below to do. You will be redirected
-          to the KYC verification page.
-          <br />
-          <br />
-          <span className="font-bold">Note:</span> KYC Verification might
-          prevent the art piece from being verified.
+          Your artwork will be verified in the next 15 Days. Thank you
         </p>
         <Button
           // onClick={() => setActiveIndex(0)}
@@ -46,6 +41,25 @@ const VerificationPending: React.FC<Props> = ({ artPiece }) => {
           Verify Now
         </Button>
       </div>
+      <div className=" flex flex-col  ">
+        {[
+          "Personal KYC",
+          "Business KYC",
+          "Physical Verification",
+          "Complete",
+        ].map((step) => (
+          <div key={step} className="flex items-center gap-8">
+            <div className="relative flex items-center justify-center">
+              <span className="w-[35px] h-[35px] absolute left-[50%] translate-x-[-50%] rounded-full bg-secondary flex items-center justify-center">
+                <GrCheckmark />
+              </span>
+              <span className="h-[80px] w-[1px] bg-primary  "></span>
+            </div>
+            <span>{step}</span>
+          </div>
+        ))}
+      </div>
+      {/* </div> */}
     </div>
   );
 };
