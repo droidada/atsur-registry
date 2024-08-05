c76fd5703a5c480b926e3e6cce8314dc733354f5
Olawoyin Daniel <olawoyindaniel95@gmail.com>
2024-08-05 01:19:51 -0700

    merged with main branch

--- src/types/models/bundle.ts
+++ src/types/models/bundle.ts
@@ -1,18 +1,17 @@
 export type BundleType = {
-    _id: string,
-    createdAt: string,
-    externalId: string;
-    details: BundleDetailType[];
-
+  _id: string;
+  createdAt: string;
+  externalId: string;
+  details: BundleDetailType[];
 };
 
 export type BundleDetailType = {
-    _id: string,
-    quantity: number,
-    item: {
-        name: string,
-        unitPrice: number,
-        externalId: string,
-        description: string;
-    };
+  _id: string;
+  quantity: number;
+  item: {
+    name: string;
+    unitPrice: number;
+    externalId: string;
+    description: string;
+  };
 };
--- src/types/models/invitationType.ts
+++ src/types/models/invitationType.ts
@@ -1,5 +1,4 @@
-export interface InviteTypeProps
-{
+export interface InviteTypeProps {
   userIsRegistered?: boolean;
   token: string;
   invitationData: any;
@@ -10,5 +9,4 @@ export interface InviteTypeProps
   rejectLoading?: boolean;
   kycVerificationStatus?: "verified" | "rejected" | "not-verified" | "pending";
   verificationData?: any;
-
 }
