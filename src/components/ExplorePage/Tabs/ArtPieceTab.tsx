374922a293a7bd7b190b6ab14b5ea1806933413c
Olawoyin Daniel <olawoyindaniel95@gmail.com>
2024-09-19 03:22:47 -0700

    making first pr for staging branch

---
+++ src/components/ExplorePage/Tabs/ArtPieceTab.tsx
@@ -0,0 +1,212 @@
+import { useState } from "react";
+import axios from "@/lib/axios";
+import {
+  Button,
+  CircularProgress,
+  Pagination,
+  Menu,
+  MenuItem,
+} from "@mui/material";
+
+import UnprotectedPage from "@/HOC/Unprotected";
+import FilterComponent from "@/components/ExplorePage/FilterComponent";
+import { IoGrid } from "react-icons/io5";
+import { IoIosSearch, IoMdMenu } from "react-icons/io";
+import { BiMenuAltLeft } from "react-icons/bi";
+import { useQuery } from "@tanstack/react-query";
+import ExploreArtPieceCard from "@/components/ExploreArtPieceCard";
+import NoData from "@/components/dashboard/NoData";
+import useDebounce from "@/hooks/useDebounce";
+import { useRouter } from "next/router";
+
+function ArtPieceTab() {
+  const [filters, setFilters] = useState<{
+    medium: string[];
+    rarity: string[];
+    priceRange: {
+      min: number;
+      max: number;
+    };
+    creationDecade: string[];
+  }>({
+    medium: [],
+    rarity: [],
+    priceRange: {
+      min: 0,
+      max: 0,
+    },
+    creationDecade: [],
+  });
+  const [currentPage, setCurrentPage] = useState(1);
+  const [rating, setRating] = useState(0);
+  const [currentQuickView, setCurrentQuickView] = useState("all");
+  const router = useRouter();
+  const { search: searchFromQuery } = router.query;
+  const [search, setSearch] = useState(searchFromQuery || "");
+  const [sort, setSort] = useState("newest");
+  const debounce = useDebounce(search as string, 500);
+
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const {
+    data: artpieces,
+    isFetching,
+    refetch,
+  } = useQuery(
+    ["artpiece", currentPage, filters, rating, debounce, sort],
+    () =>
+      axios.get(
+        `/public/explore?page=${currentPage}&filter=${JSON.stringify(
+          filters,
+        )}&rating=${rating}&search=${debounce}&sort=${sort}`,
+      ),
+    { keepPreviousData: true, refetchOnWindowFocus: false },
+  );
+
+  const handlePaginationChange = (e, value) => {
+    setCurrentPage(value);
+    refetch();
+    window.scrollTo(0, 0);
+  };
+
+  return (
+    <>
+      <div className="flex md:flex-row flex-col gap-12">
+        <FilterComponent
+          rating={rating}
+          setRating={setRating}
+          filters={filters}
+          setFilter={setFilters}
+        />
+        <div className="flex-1 page-container flex flex-col gap-5 ">
+          <div className="flex items-center justify-end gap-4">
+            <div className="flex-1">
+              <div className="flex items-center overflow-hidden divide-x-[1px] divide-primary border-[1px] border-primary rounded-[47px] h-[34px]  gap-2">
+                <input
+                  name="search"
+                  type="search"
+                  value={search}
+                  onChange={(e) => setSearch(e.target.value)}
+                  className="flex-1 h-full outline-none focus:ring-0 px-3 border-none ring-none bg-transparent"
+                  placeholder="Search"
+                />
+                <button className="px-2 h-full grid place-items-center">
+                  <IoIosSearch />
+                </button>
+              </div>
+            </div>
+            <div className="hidden md:flex gap-2 h-[34px] border-primary border-[1px] rounded-[47px] px-2">
+              <Button
+                startIcon={<IoGrid />}
+                variant="text"
+                className="text-xs font-[300] leading-[15px]"
+              >
+                Grid
+              </Button>
+              <span className="h-full w-[1px] bg-primary" />
+              <Button
+                startIcon={<IoMdMenu />}
+                variant="text"
+                className="text-xs font-[300] leading-[15px]"
+              >
+                List
+              </Button>
+            </div>
+            <div className="flex gap-2 h-[34px] border-primary border-[1px] rounded-[47px] px-2">
+              <Button
+                className="text-xs font-[300] leading-[15px]"
+                startIcon={<BiMenuAltLeft />}
+                onClick={handleClick}
+              >
+                Sort
+              </Button>
+              <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
+                <MenuItem
+                  onClick={() => {
+                    setSort("price:desc");
+                    handleClose();
+                  }}
+                >
+                  High to Low
+                </MenuItem>
+                <MenuItem
+                  onClick={() => {
+                    setSort("price:asc");
+                    handleClose();
+                  }}
+                >
+                  Low to High
+                </MenuItem>
+                <MenuItem
+                  onClick={() => {
+                    setSort("newest");
+                    handleClose();
+                  }}
+                >
+                  Newest
+                </MenuItem>
+                <MenuItem
+                  onClick={() => {
+                    setSort("oldest");
+                    handleClose();
+                  }}
+                >
+                  Oldest
+                </MenuItem>
+              </Menu>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-auto-fit items-stretch gap-2">
+            {isFetching ? (
+              <div
+                className="flex justify-center items-center h-[250px]"
+                data-aos="zoom-in" // Smooth zoom-in for loader
+              >
+                <CircularProgress color="inherit" size={30} />
+              </div>
+            ) : artpieces?.data?.artPieces?.length === 0 ? (
+              <NoData text="No Art Pieces Found" />
+            ) : (
+              artpieces?.data?.artPieces?.map((artpiece) => (
+                <>
+                  <ExploreArtPieceCard
+                    containerClassName="max-w-[350px]"
+                    link={`/explore/art-piece/${artpiece?._id}`}
+                    rating={artpiece?.rating}
+                    creator={{
+                      name: `${artpiece?.custodian?.profile?.firstName} ${artpiece?.custodian?.profile?.lastName}`,
+                      image: artpiece?.custodian?.profile?.avatar,
+                    }}
+                    image={artpiece?.assets && artpiece?.assets[0]?.url}
+                    key={artpiece?.id}
+                    title={artpiece?.title}
+                  />
+                </>
+              ))
+            )}
+          </div>
+
+          {artpieces?.data?.artPieces?.length > 0 && (
+            <div className="flex justify-center mt-12">
+              <Pagination
+                count={artpieces?.data?.meta?.totalPages}
+                page={currentPage}
+                onChange={(e, value) => handlePaginationChange(e, value)}
+              />
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ArtPieceTab;
--- src/components/ServicesPage/TrustedComponent.tsx
+++ src/components/ServicesPage/TrustedComponent.tsx
@@ -8,13 +8,19 @@ interface Props {
 const TrustedComponents: React.FC<Props> = ({ artworkData }) => {
   console.log(artworkData[0]);
   return (
-    <section className="py-12 max-w-[1000px] flex flex-col gap-4 mx-auto">
-      <h2 className="text-[47px] font-[300] text-center">
+    <section className="py-12 max-w-[1000px] flex flex-col gap-7 mx-auto">
+      <h2 className="text-[47px] font-[300] text-center" data-aos="fade-up">
         Trusted by Leading Artists
       </h2>
-      <div className="flex justify-center items-stretch gap-4 ">
+
+      <div
+        className="flex justify-center items-stretch gap-4"
+        data-aos="fade-up"
+        data-aos-delay="200"
+      >
         {artworkData?.map((item) => (
           <ArtPieceCard
+            className="max-w-[281px] w-full"
             url={`/explore/art-piece/${item._id}`}
             key={item?._id}
             title={item?.title}
@@ -27,7 +33,12 @@ const TrustedComponents: React.FC<Props> = ({ artworkData }) => {
           />
         ))}
       </div>
-      <div className="flex flex-col mt-3 items-center">
+
+      <div
+        className="flex flex-col mt-3 items-center"
+        data-aos="fade-up"
+        data-aos-delay="400"
+      >
         <h2 className="text-[26px] font-[500] text-center uppercase tracking-[17%]">
           Protect your creations today
         </h2>
@@ -36,7 +47,8 @@ const TrustedComponents: React.FC<Props> = ({ artworkData }) => {
         </p>
         <Button
           variant="contained"
-          className="max-w-[400px] w-full mt-4 h-[45px] bg-primary text-white "
+          className="max-w-[400px] w-full mt-4 h-[45px] bg-primary text-white"
+          data-aos="zoom-in"
         >
           Create
         </Button>
